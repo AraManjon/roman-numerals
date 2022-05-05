d13c1c97465f0427e628b59b2a4e15f87568ee6f
Ara <ara.manjoncb@gmail.com>
2022-05-05 11:31:47 +0200

    refactor: change ifs to check first sign to find function

--- src/index.js
+++ src/index.js
@@ -2,46 +2,17 @@ export function giveRomanNumber (arabicNumber) {
     const romanNumbers = {
         1:'I',
         2: 'II',
+        3: 'III',
         5: 'V',
         10: 'X',
         50: 'L', 
         100: 'C', 
         500: 'D',
-        1000: 'M'
-    }
-    
-
-    if (arabicNumber === 2) {
-        return romanNumbers[2]
+        1000: 'M',
     }
 
-    if (arabicNumber === 3) {
-        return romanNumbers[1] + romanNumbers[2]
-    }
-
-    if (arabicNumber === 6) {
-        return romanNumbers[5] + romanNumbers[1]
-    }
-
-    if (arabicNumber === 7) {
-        return romanNumbers[5] + romanNumbers[2]
-    }
-
-    if (arabicNumber === 8) {
-        return romanNumbers[5] + romanNumbers[1] + romanNumbers[2]
-    }
-
-    if (arabicNumber === 11) {
-        return romanNumbers[10] + romanNumbers[1]
-    }
-
-    if (arabicNumber === 12) {
-        return romanNumbers[10] + romanNumbers[2]
-    }
-    
-    if (arabicNumber === 13) {
-        return romanNumbers[10] + romanNumbers[1] + romanNumbers[2]
-    }
-
-    return romanNumbers[arabicNumber]
+    if (romanNumbers[arabicNumber]) return romanNumbers[arabicNumber]
+    const romanNums = Object.keys(romanNumbers).reverse()
+    const giveRomanNumFirstSign = romanNums.find(romanNum => arabicNumber > romanNum)
+    return romanNumbers[giveRomanNumFirstSign] + romanNumbers[arabicNumber - giveRomanNumFirstSign]
 }
