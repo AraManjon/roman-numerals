4db89bb8991ffed04132915ba373cae791542679
Ara <ara.manjoncb@gmail.com>
2022-05-05 11:52:58 +0200

    refactor: change test too add cases

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 export function giveRomanNumber (arabicNumber) {
     const romanNumbers = {
         1:'I',
-        2: 'II',
-        3: 'III',
+        4: 'IV',
         5: 'V',
+        9: 'IX',
         10: 'X',
-        50: 'L', 
+        40: 'XL',
+        50: 'L',
+        90: 'XC', 
         100: 'C', 
+        400: 'CD',
         500: 'D',
+        900: 'CM',
         1000: 'M',
     }
 
     if (romanNumbers[arabicNumber]) return romanNumbers[arabicNumber]
+
     const romanNums = Object.keys(romanNumbers).reverse()
     const giveRomanNumFirstSign = romanNums.find(romanNum => arabicNumber > romanNum)
-    return romanNumbers[giveRomanNumFirstSign] + romanNumbers[arabicNumber - giveRomanNumFirstSign]
+
+
+    return romanNumbers[giveRomanNumFirstSign] + giveRomanNumber(arabicNumber - giveRomanNumFirstSign)
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,99 +1,13 @@
 import { giveRomanNumber } from '../src/index'
 
-test('Given a 10 number when givenRomanNumber is called with this should return X', () => {
+const cases = [[10, 'X'], [1, 'I'], [5, 'V'], [50, 'L'], [2, 'II'], [3, 'III'],[6, 'VI'], [7, 'VII'], [11, 'XI'], [49, 'XLIX'], [4, 'IV'], [89, 'LXXXIX'] ]
+test.each(cases)('Given %p number when givenRomanNumber is called with this should return %p', (firstArg, secondArg) => {
+        // Arrange o Given
+        const value = firstArg
 
-    // Arrange o Given
-    const value = 10
-
-    // Act o When
-    const result = giveRomanNumber(value)
-
-    // Assert o Then
-    expect(result).toBe('X')
-})
-
-test('Given a 1 number when givenRomanNumber is called with this should return I', () => {
-
-    const value = 1
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('I')
-})
-
-test('Given a 5 number when givenRomanNumber is called with this should return V', () => {
-    const value = 5
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('V')
-})
-
-test('Given a 50 number when givenRomanNumber is called with this should return L', () => {
-    const value = 50
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('L')
-})
-
-test('Given a 2 number when givenRomanNumber is called with this should return II', () => {
-    const value = 2
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('II')
-})
-
-test('Given a 3 number when givenRomanNumber is called with this should return III', () => {
-    const value = 3
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('III')
-})
-test('Given a 6 number when givenRomanNumber is called with this should return VI', () => {
-    const value = 6
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('VI')
-})
-test('Given a 7 number when givenRomanNumber is called with this should return VII', () => {
-    const value = 7
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('VII')
-})
-
-test('Given a 8 number when givenRomanNumber is called with this should reeturn VIII', ()=> {
-    const value = 8
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('VIII')
-})
-
-test('Given a 11 number when givenRomanNumber is called with this should return XI', () => {
-    const value = 11
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('XI')
-})
-test('Given a 12 number when givenRomanNumber is called with this should return XII', () => {
-    const value = 12
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('XII')
-})
-
-test('Given a 13 number when givenRomanNumber is called with this should reeturn XIII', ()=> {
-    const value = 13
-
-    const result = giveRomanNumber(value)
-
-    expect(result).toBe('XIII')
+        // Act o When
+        const result = giveRomanNumber(value)
+    
+        // Assert o Then
+        expect(result).toBe(secondArg)
 })
