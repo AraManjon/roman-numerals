a4f94ed392cf03fa837a2a2db62d5f054bc6beb9
Ara <ara.manjoncb@gmail.com>
2022-05-06 15:58:14 +0200

    refactor: changed naming

--- src/convertToRomanNumber.js
+++ src/convertToRomanNumber.js
@@ -2,14 +2,13 @@ import { ROMAN_SYMBOL } from './romanSymbol';
 
 const findFirstRomanNum = (arabicNumber) => {
 	const romanNums = Object.keys(ROMAN_SYMBOL).reverse();
-    return romanNums.find((romanNum) => arabicNumber > romanNum);
-}
+	return romanNums.find((romanNum) => arabicNumber > romanNum);
+};
 
 export function convertToRomanNumber(arabicNumber) {
-
 	if (ROMAN_SYMBOL[arabicNumber]) return ROMAN_SYMBOL[arabicNumber];
 
-	const firstRomanNum = findFirstRomanNum(arabicNumber)
+	const firstRomanNum = findFirstRomanNum(arabicNumber);
 
 	return ROMAN_SYMBOL[firstRomanNum] + convertToRomanNumber(arabicNumber - firstRomanNum);
 }
